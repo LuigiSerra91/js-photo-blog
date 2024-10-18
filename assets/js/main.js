8183ee207b654fe6930df38c5e221e892fdfc950
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-18 12:35:30 +0200

    fix_bug_footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         let {title} = listMarkup
         let markup = `
        
-              <div id="card-hover" class="col-sm-12 col-md-6 col-lg-4 mt-5">
+              <div id="card-hover" class="col-sm-12 col-md-6 col-lg-4 g-3">
                     <div class="card rounded-0">
                         
                         <img class="card-img-top p-2" src="./assets/img/01.jpg">
